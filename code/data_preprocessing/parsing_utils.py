593cd5432a8b41aa159521cc774370785a0333d9
augustepoiroux <augustepoiroux@gmail.com>
2021-12-08 12:17:06 +0100

    Parsing now generates one encoding agnostic csv dataset

--- code/data_preprocessing/parsing_utils.py
+++ code/data_preprocessing/parsing_utils.py
@@ -8,7 +8,6 @@ Created on Tue Nov 23 14:32:26 2021
 
 import os
 import sys
-from enum import Enum
 from pathlib import Path
 from typing import Dict, List
 
@@ -17,8 +16,8 @@ import pandas as pd
 sys.path.insert(0, str(Path(__file__).parent.parent.absolute()))
 
 from tools.utils import (
-    PERIODIC_TABLE_INFO,
-    PTC_COLNAMES,
+    Encoding,
+    encode_structure,
     extract_structure_elements,
     load_json,
 )
@@ -26,53 +25,6 @@ from tools.utils import (
 DATA_DIR = os.path.join(
     str(Path(__file__).parent.parent.parent.absolute()), "data/"
 )
-
-
-class Encoding(Enum):
-    ATOMIC = "atomic"
-    COLUMN = "column"
-    COLUMN_MASS = "column_mass"
-
-
-def encode_structure(
-    df: pd.DataFrame,
-    elements_nbrs: Dict[str, int],
-    encoding: Encoding = Encoding.ATOMIC,
-):
-    total_atoms = sum(list(elements_nbrs.values()))
-    total_mass = 0.0
-    for elt, nb_elt in elements_nbrs.items():
-        elt_mass = PERIODIC_TABLE_INFO[elt]["mass"]
-        total_mass += nb_elt * elt_mass
-
-    if encoding in [Encoding.COLUMN, Encoding.COLUMN_MASS]:
-        for colname in PTC_COLNAMES:
-            df = df.assign(**{colname: 0.0})
-    elif encoding == Encoding.ATOMIC:
-        for element in PERIODIC_TABLE_INFO:
-            df = df.assign(**{element: 0.0})
-
-    for elt, nb_elt in elements_nbrs.items():
-        if encoding == Encoding.COLUMN:
-            ELEMENT_INFO = PERIODIC_TABLE_INFO[elt]
-            ptc = ELEMENT_INFO["PTC"]
-            print("-----Col encoding-----")
-            print(elt, " -> ", ptc)
-            df[ptc] = nb_elt / total_atoms
-            print("--------------------")
-        elif encoding == Encoding.COLUMN_MASS:
-            ELEMENT_INFO = PERIODIC_TABLE_INFO[elt]
-            ptc = ELEMENT_INFO["PTC"]
-            elt_mass = ELEMENT_INFO["mass"]
-            print("--Col mass encoding---")
-            print(elt, " -> ", ptc)
-            print(f"Mass of {elt}: {elt_mass:.3f}")
-            df[ptc] += nb_elt * elt_mass / total_mass
-            print("----------------------")
-        elif encoding == Encoding.ATOMIC:
-            df = df.assign(**{elt: nb_elt / total_atoms})
-
-    return df
 
 
 def compute_delta_E(df: pd.DataFrame):
@@ -114,14 +66,53 @@ def parse_json(
         )
 
 
-if __name__ == "__main__":
-    p = Path(DATA_DIR)
+def parse_all_json(
+    data_dir: str, savepath: str, inv_k_density: bool = False,
+) -> pd.DataFrame:
+    list_df = []
+    p = Path(data_dir)
     for struct_dir in p.iterdir():
         if not struct_dir.is_dir():
             continue
         for file in struct_dir.glob("data.json"):
             structure_name = os.path.basename(struct_dir)
 
+            print(f"Parsing {structure_name}...")
+
+            data = load_json(file)
+
+            df = pd.DataFrame(data)
+            df = compute_delta_E(df)
+
+            if inv_k_density:
+                df["k_density"] = df["k_density"].apply(
+                    lambda x: int(round(1 / x))
+                )
+
+            df["structure"] = structure_name
+            df = df[["structure"] + df.columns.tolist()[:-1]]
+            list_df.append(df)
+
+            print("Done!\n")
+
+    res = pd.concat(list_df, ignore_index=True)
+    res.to_csv(os.path.join(savepath, "data.csv"))
+
+
+if __name__ == "__main__":
+    parse_all_json(
+        DATA_DIR, DATA_DIR, inv_k_density=True,
+    )
+    exit()
+
+    # Code below parse JSON individually and with specific encodings
+    p = Path(DATA_DIR)
+    for struct_dir in p.iterdir():
+        if not struct_dir.is_dir():
+            continue
+        for file in struct_dir.glob("data.json"):
+            structure_name = os.path.basename(struct_dir)
+            print(f"Parsing {structure_name}...")
             # Parsing the structure name to get the elements and their number
             elements_nbrs = extract_structure_elements(structure_name)
 
--- code/tools/utils.py
+++ code/tools/utils.py
@@ -2,8 +2,10 @@ import json
 import os
 import re
 from collections import defaultdict
+from enum import Enum
 from typing import Dict
 
+import pandas as pd
 from natsort import natsorted
 
 
@@ -11,6 +13,12 @@ def load_json(filepath: str):
     with open(filepath) as file:
         data = json.load(file)
     return data
+
+
+class Encoding(Enum):
+    ATOMIC = "atomic"
+    COLUMN = "column"
+    COLUMN_MASS = "column_mass"
 
 
 PERIODIC_TABLE_INFO = load_json(
@@ -34,3 +42,82 @@ def extract_structure_elements(structure_name: str) -> Dict[str, int]:
         else:
             elements_nbrs[elt[0]] += int(elt[1])
     return dict(elements_nbrs)
+
+
+def encode_structure(
+    df: pd.DataFrame, elements_nbrs: Dict[str, int], encoding: Encoding,
+):
+    total_atoms = sum(list(elements_nbrs.values()))
+    total_mass = 0.0
+    for elt, nb_elt in elements_nbrs.items():
+        elt_mass = PERIODIC_TABLE_INFO[elt]["mass"]
+        total_mass += nb_elt * elt_mass
+
+    if encoding in [Encoding.COLUMN, Encoding.COLUMN_MASS]:
+        for colname in PTC_COLNAMES:
+            df = df.assign(**{colname: 0.0})
+    elif encoding == Encoding.ATOMIC:
+        for element in PERIODIC_TABLE_INFO:
+            df = df.assign(**{element: 0.0})
+
+    for elt, nb_elt in elements_nbrs.items():
+        if encoding == Encoding.COLUMN:
+            ELEMENT_INFO = PERIODIC_TABLE_INFO[elt]
+            ptc = ELEMENT_INFO["PTC"]
+            print("-----Col encoding-----")
+            print(elt, " -> ", ptc)
+            df[ptc] = nb_elt / total_atoms
+            print("--------------------")
+        elif encoding == Encoding.COLUMN_MASS:
+            ELEMENT_INFO = PERIODIC_TABLE_INFO[elt]
+            ptc = ELEMENT_INFO["PTC"]
+            elt_mass = ELEMENT_INFO["mass"]
+            print("--Col mass encoding---")
+            print(elt, " -> ", ptc)
+            print(f"Mass of {elt}: {elt_mass:.3f}")
+            df[ptc] += nb_elt * elt_mass / total_mass
+            print("----------------------")
+        elif encoding == Encoding.ATOMIC:
+            df = df.assign(**{elt: nb_elt / total_atoms})
+
+    return df
+
+
+def encode_all_structures(
+    df: pd.DataFrame, encoding: Encoding,
+):
+    if encoding in [Encoding.COLUMN, Encoding.COLUMN_MASS]:
+        for colname in PTC_COLNAMES:
+            df = df.assign(**{colname: 0.0})
+    elif encoding == Encoding.ATOMIC:
+        for element in PERIODIC_TABLE_INFO:
+            df = df.assign(**{element: 0.0})
+
+    for structure_name in df["structure"].unique():
+        elements_nbrs = extract_structure_elements(structure_name)
+        total_atoms = sum(list(elements_nbrs.values()))
+        total_mass = 0.0
+        for elt, nb_elt in elements_nbrs.items():
+            elt_mass = PERIODIC_TABLE_INFO[elt]["mass"]
+            total_mass += nb_elt * elt_mass
+
+        for elt, nb_elt in elements_nbrs.items():
+            if encoding == Encoding.COLUMN:
+                ELEMENT_INFO = PERIODIC_TABLE_INFO[elt]
+                ptc = ELEMENT_INFO["PTC"]
+                df.loc[df["structure"] == structure_name, ptc] = (
+                    nb_elt / total_atoms
+                )
+            elif encoding == Encoding.COLUMN_MASS:
+                ELEMENT_INFO = PERIODIC_TABLE_INFO[elt]
+                ptc = ELEMENT_INFO["PTC"]
+                elt_mass = ELEMENT_INFO["mass"]
+                df.loc[df["structure"] == structure_name, ptc] = (
+                    nb_elt * elt_mass / total_mass
+                )
+            elif encoding == Encoding.ATOMIC:
+                df.loc[df["structure"] == structure_name, elt] = (
+                    nb_elt / total_atoms
+                )
+
+    return df
