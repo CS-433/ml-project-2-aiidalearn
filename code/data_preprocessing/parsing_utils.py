6e95c6222ce0ab3303352cef144055afda9a8dc5
augustepoiroux <augustepoiroux@gmail.com>
2021-12-09 19:31:01 +0100

    Some cleanup and refactorization

--- code/data_preprocessing/parsing_utils.py
+++ code/data_preprocessing/parsing_utils.py
@@ -9,22 +9,15 @@ Created on Tue Nov 23 14:32:26 2021
 import os
 import sys
 from pathlib import Path
-from typing import Dict, List
 
 import pandas as pd
-
-from rich.columns import Columns
 from rich.console import Console
 from rich.panel import Panel
+from rich.progress import track
 
 sys.path.insert(0, str(Path(__file__).parent.parent.absolute()))
 
-from tools.utils import (
-    Encoding,
-    encode_structure,
-    extract_structure_elements,
-    load_json,
-)
+from tools.utils import load_json
 
 DATA_DIR = os.path.join(
     str(Path(__file__).parent.parent.parent.absolute()), "data/"
@@ -42,32 +35,10 @@ def compute_delta_E(df: pd.DataFrame):
             idx_ref = idx
 
     ref_energy = df.loc[df["converged"], "total_energy"][idx_ref]
-    print(f"Ref energy: {ref_energy} (found at index {idx_ref})")
 
     df = df.assign(delta_E=df["total_energy"] - ref_energy)
 
-    return df
-
-
-def parse_json(
-    filepath: str,
-    savepath: str,
-    elements_nbrs: Dict[str, int],
-    encodings: List[Encoding] = [Encoding.ATOMIC],
-    inv_k_density: bool = False,
-):
-    data = load_json(filepath)
-
-    df = pd.DataFrame(data)
-    df = compute_delta_E(df)
-
-    if inv_k_density:
-        df["k_density"] = df["k_density"].apply(lambda x: int(round(1 / x)))
-
-    for encoding in encodings:
-        encode_structure(df.copy(), elements_nbrs, encoding).to_csv(
-            os.path.join(savepath, f"enc_{encoding.value}.csv")
-        )
+    return df, ref_energy
 
 
 def parse_all_json(
@@ -75,59 +46,64 @@ def parse_all_json(
 ) -> pd.DataFrame:
     list_df = []
     p = Path(data_dir)
+    nb_folders = len(os.listdir(data_dir))
     console = Console()
-    p = Path(DATA_DIR)
-    with console.status("[bold blue] Parsing structures...") as status:
-        for struct_dir in p.iterdir():
-            if not struct_dir.is_dir():
-                continue
-            for file in struct_dir.glob("data.json"):
-                structure_name = os.path.basename(struct_dir)
+    for struct_dir in track(
+        p.iterdir(),
+        description="Parsing structures...",
+        total=nb_folders,
+        console=console,
+    ):
+        if not struct_dir.is_dir():
+            continue
+        for file in struct_dir.glob("data.json"):
+            structure_name = os.path.basename(struct_dir)
 
-                data = load_json(file)
+            df = pd.DataFrame(load_json(file))
+            df, ref_energy = compute_delta_E(df)
 
-                df = pd.DataFrame(data)
-                df = compute_delta_E(df)
+            if inv_k_density:
+                df["k_density"] = df["k_density"].apply(
+                    lambda x: int(round(1 / x))
+                )
 
-                if inv_k_density:
-                    df["k_density"] = df["k_density"].apply(
-                        lambda x: int(round(1 / x))
-                    )
+            df["structure"] = structure_name
+            df = df[["structure"] + df.columns.tolist()[:-1]]
+            list_df.append(df)
 
-                df["structure"] = structure_name
-                df = df[["structure"] + df.columns.tolist()[:-1]]
-                list_df.append(df)
-
-                console.print(f'[blue]Parsed {structure_name}')
+            console.print(
+                Panel(
+                    "[blue]"
+                    f"Reference energy: {ref_energy:.2f}\n"
+                    f"Nb simulations: {len(df)}",
+                    title=structure_name,
+                    expand=False,
+                )
+            )
 
     res = pd.concat(list_df, ignore_index=True)
-    res.to_csv(os.path.join(savepath, "data.csv"))
+
+    # print a summary on the collected data
+    console.print(
+        Panel(
+            "[blue]"
+            f"{res.structure.nunique()} unique structures\n"
+            f"{res.shape[0]} total simulations\n"
+            f"{res.loc[res['converged']].shape[0]} converged simulations",
+            title="Data summary",
+            expand=False,
+            style="bold green",
+        )
+    )
+
+    with console.status("") as status:
+        # save the data
+        status.update(f"Saving parsed data to {savepath}...")
+        res.to_csv(savepath)
+        console.log(f"Data stored in {savepath}")
 
 
 if __name__ == "__main__":
     parse_all_json(
-        DATA_DIR, DATA_DIR, inv_k_density=True,
+        DATA_DIR, os.path.join(DATA_DIR, "data.csv"), inv_k_density=True,
     )
-    exit()
-
-    # Code below parses JSON individually and with specific encodings
-    console = Console()
-    p = Path(DATA_DIR)
-    with console.status("[bold blue] Parsing structures...") as status:
-        for struct_dir in p.iterdir():
-            if not struct_dir.is_dir():
-                continue
-            for file in struct_dir.glob("data.json"):
-                structure_name = os.path.basename(struct_dir)
-                # Parsing the structure name to get the elements and their number
-                elements_nbrs = extract_structure_elements(structure_name)
-
-                parse_json(
-                    filepath=file,
-                    savepath=struct_dir,
-                    elements_nbrs=elements_nbrs,
-                    encodings=list(Encoding),
-                    inv_k_density=True,
-                )
-
-                console.print(f'[blue]Parsed {structure_name}')
--- code/regression/sim_time.py
+++ code/regression/sim_time.py
@@ -1,144 +1,167 @@
 import os
+import pickle
+import sys
+
+from pathlib import Path
 import numpy as np
 import pandas as pd
-import sys
-from time import sleep
-import pickle
-
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import StandardScaler, PolynomialFeatures
-from sklearn.linear_model import LinearRegression
-from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error, mean_absolute_percentage_error, mean_absolute_error
-
 import xgboost as xgb
-
-from rich.columns import Columns
 from rich.console import Console
-from rich.panel import Panel
+from rich.table import Table
+from sklearn.ensemble import GradientBoostingRegressor, RandomForestRegressor
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import (
+    mean_absolute_error,
+    mean_absolute_percentage_error,
+    mean_squared_error,
+)
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import PolynomialFeatures, StandardScaler
 
-# Set up
-sys.path.append(os.path.dirname(os.getcwd()))
-from tools.utils import encode_all_structures, Encoding
-
-DATA_DIR = os.path.join(
-    os.path.dirname(os.path.dirname(os.getcwd())), "data/"
+sys.path.append(str(Path(__file__).parent.parent.absolute()))
+from tools.utils import (
+    Encoding,
+    custom_mape,
+    encode_all_structures,
 )
 
-# Loading Data
+# Set Up
+DATA_DIR = os.path.join(
+    str(Path(__file__).parent.parent.parent.absolute()), "data/"
+)
+
+MODELS_DIR = os.path.join(
+    str(Path(__file__).parent.parent.parent.absolute()), "models/delta_E/"
+)
+
 encoding = Encoding.COLUMN_MASS
 
-df = pd.read_csv(os.path.join(DATA_DIR, "data.csv"), index_col=0, na_filter=False)
-df = encode_all_structures(df, encoding)
+console = Console()
 
-cols_raw = list(df.columns)
-cols_trash = ["structure", 'converged', "n_iterations", "delta_E", "fermi", "total_energy"]
-cols_independent = ['time']
-cols_drop = cols_trash + cols_independent
+# Loading Data
+with console.status("") as status:
+    status.update("[bold blue]Loading data...")
+    df = pd.read_csv(
+        os.path.join(DATA_DIR, "data.csv"), index_col=0, na_filter=False
+    )
 
-cols_dependent = cols_raw.copy()
-for element in cols_drop:
-    cols_dependent.remove(element)
+    status.update(f"[bold blue]Encoding structures ({encoding})...")
+    df = encode_all_structures(df, encoding)
 
-X_raw = df[cols_dependent][df["converged"]]
-y_raw = np.abs(df[cols_independent][df["converged"]]).squeeze()
+    status.update(f"[bold blue]Splitting data...")
+    cols_raw = list(df.columns)
+    cols_trash = [
+        "structure",
+        "converged",
+        "n_iterations",
+        "delta_E",
+        "fermi",
+        "total_energy",
+    ]
+    cols_independent = ["time"]
+    cols_drop = cols_trash + cols_independent
 
-# Train-test-split
-X_train, X_test, y_train, y_test = train_test_split(
-    X_raw, y_raw,
-    test_size=0.2,
-    random_state=42
-)
+    cols_dependent = cols_raw.copy()
+    for element in cols_drop:
+        cols_dependent.remove(element)
+
+    X_raw = df[cols_dependent][df["converged"]]
+    y_raw = np.abs(df[cols_independent][df["converged"]]).squeeze()
+
+    # Train-test-split
+    X_train, X_test, y_train, y_test = train_test_split(
+        X_raw, y_raw, test_size=0.2, random_state=42
+    )
+console.log("Data loaded")
 
 # Model definition
-linear_augmented_model = Pipeline([
-    ('scaler_init', StandardScaler()),
-    ('poly_features', PolynomialFeatures(degree=2)),
-    ('scaler_final', StandardScaler()),
-    ('regressor', LinearRegression()),
-])
+linear_augmented_model = Pipeline(
+    [
+        ("scaler_init", StandardScaler()),
+        ("poly_features", PolynomialFeatures(degree=2)),
+        ("scaler_final", StandardScaler()),
+        ("regressor", LinearRegression()),
+    ]
+)
 
 rf_model = RandomForestRegressor(random_state=0)
 
-gb_model = GradientBoostingRegressor(n_estimators=5000, learning_rate=0.05, random_state=0)
+gb_model = GradientBoostingRegressor(
+    n_estimators=5000, learning_rate=0.05, random_state=0
+)
 
-xgb_model = xgb.XGBRegressor(n_estimators=5000, learning_rate=0.05, random_state=0)
+xgb_model = xgb.XGBRegressor(
+    n_estimators=5000, learning_rate=0.05, random_state=0
+)
+
+# detect if gpu is usable with xgboost by training on toy data
+try:
+    xgb_model.set_params(tree_method="gpu_hist")
+    xgb_model.fit(np.array([[1, 2, 3]]), np.array([[1]]))
+    console.print("[italic bright_black]Using GPU for XGBoost")
+except:
+    xgb_model.set_params(tree_method="hist")
+    console.print("[italic bright_black]Using CPU for XGBoost")
 
 models = {
     "Augmented Linear Regression": linear_augmented_model,
     "Random Forest": rf_model,
-    "Gradient Boosting": gb_model,
+    # "Gradient Boosting": gb_model,
     "XGBoost": xgb_model,
 }
 
 # Model training
-
-console = Console()
-with console.status("[bold blue]Training models...") as status:
+with console.status("") as status:
     for model_name, model in models.items():
+        status.update(f"[bold blue]Training {model_name}...")
         model.fit(X_train, y_train)
         console.log(f"[blue]Finished training {model_name}[/blue]")
 
 
 # Model evaluation
-
-with console.status("[bold blue] Evaluating models...") as status:
+with console.status("") as status:
     for model_name, model in models.items():
-        sleep(0.5)
-        console.log(f"[blue]Evaluating {model_name}[/blue]")
+        status.update(f"[bold blue]Evaluating {model_name}...")
+
+        table = Table(title=model_name)
+        table.add_column("Loss name", justify="center", style="cyan")
+        table.add_column("Train", justify="center", style="green")
+        table.add_column("Test", justify="center", style="green")
 
         y_pred_train = model.predict(X_train)
         y_pred_test = model.predict(X_test)
 
-        mse_test = mean_squared_error(y_test, y_pred_test)
-        mse_train = mean_squared_error(y_train, y_pred_train)
+        for loss_name, loss_fn in [
+            ("MSE", mean_squared_error),
+            ("MAE", mean_absolute_error),
+            ("MAPE", mean_absolute_percentage_error),
+            # ("Custom MAPE", custom_mape),
+        ]:
+            train_loss = loss_fn(y_train, y_pred_train)
+            test_loss = loss_fn(y_test, y_pred_test)
+            table.add_row(loss_name, f"{train_loss:.4E}", f"{test_loss:.4E}")
 
-        titlestr = f'MSE\n'
-        contentstr = f"train:{mse_train:.4E}\ttest:{mse_test:.4E}"
-        p1 = Panel(titlestr+contentstr)
+        console.print(table)
 
-        mae_test = mean_absolute_error(y_test, y_pred_test)
-        mae_train = mean_absolute_error(y_train, y_pred_train)
+if input("Save models? (y/[n]) ") == "y":
+    save_models = {
+        "Random Forest": rf_model,
+        "Gradient Boosting": gb_model,
+        "XGBoost": xgb_model,
+    }
 
-        titlestr = f'MAE\n'
-        contentstr = f"train:{mae_train:.4E}\ttest:{mae_test:.4E}"
-        p2 = Panel(titlestr + contentstr)
+    models_filenames = [
+        "random_forest_model.pkl",
+        "gb_model.pkl",
+        "xgb_model.pkl",
+    ]
 
-        mape_test = mean_absolute_percentage_error(y_test, y_pred_test)
-        mape_train = mean_absolute_percentage_error(y_train, y_pred_train)
-
-        titlestr = f'MAPE\n'
-        contentstr = f"train:{mape_train:.4E}\ttest:{mape_test:.4E}"
-        p3 = Panel(titlestr+contentstr)
-
-        renderables = [p1, p2, p3]
-        console.print(Columns(renderables))
-
-        # mape_test = custom_mape(y_test, y_pred_test)
-        # mape_train = custom_mape(y_train, y_pred_train)
-        # print(f"Custom MAPE:\ttrain:{mape_train:.4E}\ttest:{mape_test:.4E}")
-
-MODELS_DIR = os.path.join(
-    os.path.dirname(os.path.dirname(os.getcwd())), "models/sim_time/"
-)
-
-save_models = {
-    "Random Forest": rf_model,
-    "Gradient Boosting": gb_model,
-    "XGBoost": xgb_model,
-}
-
-models_filenames = [
-    "random_forest_model.pkl",
-    "gb_model.pkl",
-    "xgb_model.pkl",
-]
-
-with console.status("[bold green] Saving models...") as status:
-    for filename, (model_name, model) in zip(models_filenames, save_models.items()):
-        sleep(1)
-        modelpath = MODELS_DIR + filename
-        with open(modelpath, "wb") as file:
-            pickle.dump(model, file)
-        console.log(f"[green]Saved {model_name} to {modelpath}[/green]")
+    with console.status("[bold green] Saving models...") as status:
+        for filename, (model_name, model) in zip(
+            models_filenames, save_models.items()
+        ):
+            modelpath = MODELS_DIR + filename
+            with open(modelpath, "wb") as file:
+                pickle.dump(model, file)
+            console.log(f"[green]Saved {model_name} to {modelpath}[/green]")
